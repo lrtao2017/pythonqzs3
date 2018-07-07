0bf90b97150120bd40c17a15a0df635805ffa85b
lrtao2010 <lrtao2010@163.com>
2018-07-07 11:43:27 +0800

    201807071143

--- app/method/str_method.py
+++ app/method/str_method.py
@@ -2,25 +2,67 @@
 #Python 3 字符串常用方法
 __author__ = "lrtao2010"
 
-#capitalize 首字母大写
+#capitalize 将字符串的首字符改为大写
 # a = "lrtao"
 # v = a.capitalize()
 # print(v)
 # Lrtao
 
-#casefold 全部变小写
+#casefold 将字符串所有字符改为小写，功能强大
 # a = "LrTao"
 # v = a.casefold()
 # print(v)
 # lrtao
 
-#center(self, width, fillchar=None) 内容居中，width：总长度；fillchar：空白处填充内容，默认无
+#lower 将字符串所有字符改为小写
+# a = "LrTao"
+# v = a.lower()
+# print(v)
+# lrtao
+
+#upper 将字符全部转成大写
+# a = "LrTao"
+# v = a.upper()
+# print(v)
+# LRTAO
+
+#center(self, width, fillchar=None) 内容居中，width：总长度（当width<len(s)时没有效果）；
+#                                   fillchar：空白处填充内容，默认无
 # a = "lrtao"
 # v = a.center(20,'*')
 # print(v)
 # *******lrtao********
 
-#count(self, sub, start=None, end=None) 统计子序列个数,包括start，不包括end
+#ljust(self, width: int, fillchar: str = ...)内容左对齐，右侧填充,不指定默认空白
+# a = "lrtao"
+# v1 = a.ljust(20,'*')
+# v2 = a.ljust(20)
+# print(v1)
+# print(v2)
+# lrtao***************
+# lrtao
+
+#rjust(self, width: int, fillchar: str = ...)内容右对齐，左侧填充,不指定默认空白
+# a = "lrtao"
+# v1 = a.rjust(20,'*')
+# v2 = a.rjust(20)
+# print(v1)
+# print(v2)
+# ***************lrtao
+#                lrtao
+
+# zfill(self, width: int) z指zero，用0将字符填充到指定长度(左侧填充)
+# a = "test"
+# v = a.zfill(20)
+# print(v)
+# 0000000000000000test
+
+# a = "test"
+# v1 = a.rjust(20,'0')
+# print(v1)
+# 0000000000000000test
+
+#count(self, sub, start=None, end=None) 统计子序列在字符串里出现的次数,包括start，不包括end
 
 # a = "lilelili"
 # v1 = a.count('li')
@@ -28,7 +70,11 @@ __author__ = "lrtao2010"
 # v3 = a.count('li',1,6)
 # print(v1,v2,v3)
 # 3 2 1
-#encode  编码
+#encode(encoding='utf-8',errors='strict')  以encoding指定的编码格式对字符串进行编码
+# a = "lilei"
+# v = a.encode(encoding='utf-8')
+# print(v)
+# b'lilei'
 #
 
 #endswith(self, suffix, start=None, end=None)是否以指定字符串结束,返回值为布尔值
@@ -42,7 +88,22 @@ __author__ = "lrtao2010"
 # print(v1,v2,v3,v4)
 # True False True True
 
-#expandtabs  将tab转换成空格，如果tab转换空格数量没有定义，默认一个tab转换成8个空格
+#startswith(self, prefix: Union[str, Tuple[str, ...]], start: Optional[int] = ...,end: Optional[int] = ...)
+#是否以指定字符串开始,返回值为布尔值
+# a = "hello"
+# v1 = a.startswith('h')
+# v2 = a.startswith('e')
+# v3 = a.startswith('he')
+# v4 = a.startswith('e',1,3)
+#
+# print(v1,v2,v3,v4)
+# True False True True
+
+#expandtabs  把字符串的tab字符（\t）转化为空格，如果tab转换空格数量没有定义，默认一个tab转换成8个空格
+# a = 'I\tam\ttom'
+# v = a.expandtabs(4)
+# print(v)
+# I   am  tom
 
 # find(self, sub, start=None, end=None) 返回首次找到子序列的位置，如果没找到，返回 -1
 # a = 'lilei'
@@ -52,14 +113,37 @@ __author__ = "lrtao2010"
 # print(v1,v2,v3)
 # 0 2 -1
 
-#index(self, sub, start=None, end=None)返回首次找到子序列的位置，如果没找到程序会报错
+#index(self, sub, start=None, end=None)返回首次找到子序列的位置，如果没找到程序会报异常
 # a = 'lilei'
 # v = a.index('ll')
 # print(v)
 # Traceback (most recent call last):
-#   File "D:/python3/app/method/str_method.py", line 58, in <module>
 #     v = a.index('ll')
 # ValueError: substring not found
+
+#rfind(self, sub: str, __start: Optional[int] = ..., __end: Optional[int] = ...)
+#返回指定子串的最大索引，如果没找到则返回-1，可以指定要开始和结束位置。
+# a = 'lilililili'
+# v1 = a.rfind('li')
+# v2 = a.rfind('li',9)
+# v3 = a.rfind('li',2,4)
+# print(v1,v2,v3)
+# 8 -1 2
+
+#rindex(self, sub: str, __start: Optional[int] = ..., __end: Optional[int] = ...)
+#返回指定子串的最大索引，如果没找到则抛异常，可以指定要开始和结束位置。
+# a = 'lilililili'
+# v1 = a.rindex('li')
+# v2 = a.rindex('li',9)
+# v3 = a.rindex('li',2,4)
+# print(v1)
+# print(v2)
+# print(v3)
+# v2 = a.rindex('li',9)
+# ValueError: substring not found
+#
+# Process finished with exit code 1
+
 
 #format 字符串格式化
 # a = 'hello ,{name}!hello ,{name2} !'
@@ -161,27 +245,208 @@ __author__ = "lrtao2010"
 # Error: byte数字（单字节）
 
 
-#isidentifier 如果字符串是有效的Python标识符，则返回True，否则返回False
-# a = '_123'
-# b = '123'
-# c = 'a123'
-# d = '#123'
-# v1 = a.isidentifier()
-# v2 = b.isidentifier()
-# v3 = c.isidentifier()
-# v4 = d.isidentifier()
-# print(v1,v2,v3,v4)
-# True False True False
+#isidentifier 判断字符串是否包含python的保留字
+# v1 = 'def'.isidentifier()
+# v2 = 'aaa'.isdecimal()
+# print(v1,v2)
+# True False
 
 #islower 字符串中不能出现大写字母，并且至少有一个小写字母
 # a = 'ls12#3'
 # b = 'Ls'
 # c = '1234s'
-# d = '1234'
+# d = 'LS'
 # v1 = a.islower()
 # v2 = b.islower()
 # v3 = c.islower()
 # v4 = d.islower()
 # print(v1,v2,v3,v4)
 # True False True False
+
+#isprintable 判断字符串中所有的字符串都是可以通过repr表示成字符串，
+#            或者字符串是空的，都返回True,否则返回False
+# v = chr(33).isprintable()
+# v1 = chr(1000000).isprintable()
+# print(v,v1)
+# True False
+
+#isspace 判断字符串是否是空格
+# a = ''
+# b = ' '
+# v1 = a.isspace()
+# v2 = b.isspace()
+# print(v1,v2)
+# False True
+
+#title  标题格式(可以理解为单词首字母大写，其它字符小写)
+# a = 'this is title'
+# v = a.title()
+# print(v)
+# This Is Title
+
+#istitle 判断是否是标题格式（可以理解为首字母是否大写）
+# a = "this is title"
+# b = "This is title"
+# c = "This Is title"
+# d = "This Is Title"
+# v1 = a.istitle()
+# v2 = b.istitle()
+# v3 = c.istitle()
+# v4 = d.istitle()
+# print(v1,v2,v3,v4)
+# False False False True
+
+#isupper 判断字母是否全部是大写
+# a = 'lsL123'
+# b = 'LSF123'
+# c = 'LSF'
+# d = 'LS F'
+# v1 = a.isupper()
+# v2 = b.isupper()
+# v3 = c.isupper()
+# v4 = d.isupper()
+# print(v1,v2,v3,v4)
+# False True True True
+
+#join(self, iterable: Iterable[str])返回一个用指定分隔符分隔的字，
+#                                   或者是将指定字符加入到另一个字符中。
+# a = 'iamtom'
+# v1 = '.'.join(a)
+# v2 = '#'.join(a)
+# print(v1)
+# print(v2)
+# i.a.m.t.o.m
+# i#a#m#t#o#m
+
+#lstrip 移除左侧空白(没有测试成功)
+
+#partition(self, sep: str) 按照指定的字符将字符串分为前中后三部分,从左侧开始
+# a = "iamtom"
+# v1 = a.partition("i")
+# v2 = a.partition('am')
+# v3 = a.partition('om')
+# v4 = a.partition('som')
+# print(v1)
+# print(v2)
+# print(v3)
+# print(v4)
+# ('', 'i', 'amtom')
+# ('i', 'am', 'tom')
+# ('iamt', 'om', '')
+# ('iamtom', '', '')
+
+#rpartition(self, sep: str) 与partition一样，但是从右边开始
+# a = "iamtom"
+# v1 = a.rpartition("i")
+# v2 = a.rpartition('am')
+# v3 = a.rpartition('tom')
+# v4 = a.rpartition('som')
+# print(v1)
+# print(v2)
+# print(v3)
+# print(v4)
+# ('', 'i', 'amtom')
+# ('i', 'am', 'tom')
+# ('iam', 'tom', '')
+# ('', '', 'iamtom')
+
+
+#replace(self, old: str, new: str, count: int = ...) 替换字符，不指定次数默认全部替换
+# a = 'iamtomtomtom'
+# v1 = a.replace('m','i')
+# v2 = a.replace('m','i',1)
+# print(v1)
+# print(v2)
+# iaitoitoitoi
+# iaitomtomtom
+
+
+#split(self, sep: Optional[str] = ..., maxsplit: int = ...)
+#按指定字符串从左侧开始进行切割，可以指定切割次数(不指定全部切割)
+
+# a = 'iamtomtom'
+# v1 = a.split('m')
+# v2 = a.split('m',2)
+# print(v1)
+# print(v2)
+# ['ia', 'to', 'to', '']
+# ['ia', 'to', 'tom']
+
+#rsplit(self, sep: Optional[str] = ..., maxsplit: int = ...)
+#按指定字符串从右侧开始进行切割，可以指定切割次数(不指定全部切割)
+# a = 'iamtomtom'
+# v1 = a.rsplit('m')
+# v2 = a.rsplit('m',2)
+# print(v1)
+# print(v2)
+# ['ia', 'to', 'to', '']
+# ['iamto', 'to', '']
+
+#strip(self, chars: Optional[str] = ...)
+#移除两侧（最外侧）指定字符串，默认移除空格，以指定多个字符
+
+# a = ' i am tom m'
+# v1 = a.strip()
+# v2 = a.strip('m')
+# v3 = a.strip('ap')
+# v4 = a.strip('am')
+# print(v1)
+# print(v2)
+# print(v3)
+# print(v4)
+# i am tom m
+#  i am tom
+#  i am tom m
+#  i am tom
+
+#rstrip(self, chars: Optional[str] = ...)
+#移除右侧指定字符
+
+# a = ' i am tom m'
+# v1 = a.rstrip()
+# v2 = a.rstrip('m')
+# v3 = a.rstrip('ap')
+# v4 = a.rstrip('am')
+# print(v1)
+# print(v2)
+# print(v3)
+# print(v4)
+ # i am tom m
+ # i am tom
+ # i am tom m
+ # i am tom
+
+ #splitlines(self, keepends: bool = ...)
+ #按换行符\n切割，如果没指定keepends=True，则会将其从结果中移除
+# a = "this is test1\n this is test2"
+# v1 = a.splitlines(keepends=True)
+# v2 = a.splitlines()
+# print(v1)
+# print(v2)
+# ['this is test1\n', ' this is test2']
+# ['this is test1', ' this is test2']
+
+#swapcase 大小写转换
+# a = 'This Is Test 01'
+# v = a.swapcase()
+# print(v)
+# tHIS iS tEST 01
+
+#translate(self, table: Optional[bytes], delete: bytes = ...)
+#translate() 方法根据参数table给出的表(包含 256 个字符)转换字符串的字符,
+# 要过滤掉的字符放到 deletechars 参数中
+
+# intab = "aeiou"
+# outtab = "12345"
+# trantab = str.maketrans(intab, outtab)  # 制作翻译表
+# str = "this is string example....wow!!!"
+# print(str.translate(trantab))
+# th3s 3s str3ng 2x1mpl2....w4w!!!
+
+# 制作翻译表
+# bytes_tabtrans = bytes.maketrans(b'abcdefghijklmnopqrstuvwxyz', b'ABCDEFGHIJKLMNOPQRSTUVWXYZ')
+#
+# # 转换为大写，并删除字母o
+# print(b'runoob'.translate(bytes_tabtrans, b'o'))
+# b'RUNB'
 
