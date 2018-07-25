e635f4ae4a3341667259f1ae1c6d1ca85d5931ee
lrtao2010 <lrtao2010@163.com>
2018-07-25 17:04:16 +0800

    20180725

--- app/task/menu/menu2.py
+++ app/task/menu/menu2.py
@@ -47,20 +47,35 @@ menu = {
 }
 
 des_name_dict = menu
-des_name_dict_old = {}
+key_list = []
 no_quit = True
 while no_quit:
     for key in des_name_dict.keys():
         print(key) 
     des_name = input('> b or q').strip()
     if not des_name:continue
-    elif des_name == 'b':
-        pass
     elif des_name in des_name_dict.keys():
+        key_list.append(des_name)
+        print(key_list)
         des_name_dict = des_name_dict[des_name]
         if des_name_dict == {}:
             print("已到达最底层，请返回上一层")
-        else:
+        continue
+    elif des_name == 'b':
+        if len(key_list) == 0:
+            des_name_dict = menu
+            continue
+        elif len(key_list) == 1:
+            key_list.pop()
+            des_name_dict = menu
+            continue
+        elif len(key_list) == 2:
+            key_list.pop()
+            des_name_dict = menu[key_list[0]]
+            continue
+        elif len(key_list) == 3:
+            key_list.pop()
+            des_name_dict = menu[key_list[0]][key_list[1]]
             continue
     elif des_name == 'q':
         no_quit = False
