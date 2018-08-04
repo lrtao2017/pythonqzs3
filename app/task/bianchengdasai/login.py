9270a293286b16386cc13d74e73d2b49df35c860
lrtao2010 <lrtao2010@163.com>
2018-08-04 11:27:14 +0800

    add+2 login

--- app/task/bianchengdasai/login.py
+++ app/task/bianchengdasai/login.py
@@ -9,7 +9,10 @@ name_passwd = {
 locked_account_file = "lock.txt"
 error_count = 0
 allow_login = True
+account_is_no_locked = True
+login_successful = False
 
+#登录系统
 while allow_login :
     user_name = input("请输入用户名：" ).strip().lower()
     user_password = input("请输入密码：")
@@ -23,6 +26,7 @@ while allow_login :
                     #!=[]防止locked_account_file中有空行报错
                     print("%s 账号已经被锁定，请联系管理员解锁账号" % user_name)
                     allow_login = False
+                    account_is_no_locked = False
 
 
         user_name_old = user_name
@@ -33,11 +37,12 @@ while allow_login :
         continue
 
     # 验证账号密码
-    if allow_login == True:
+    if account_is_no_locked :
         if user_name in name_passwd.keys():
             if user_password == name_passwd[user_name][0]:
                 print('\033[1;36m%s %s 您好，欢迎光临!\033[0m' % (name_passwd[user_name][1],user_name))
                 allow_login = False
+                login_successful = True
             else:
                 print("用户名或密码错误")
                 error_count += 1
@@ -46,9 +51,13 @@ while allow_login :
                 # 存在的账号密码错误三次被锁定
                 if error_count == 3:
                     with open(locked_account_file, 'a') as user_lock:
-                        user_lock.write(user_name + '\n')
+                        user_lock.write(user_name.strip() + '\n')
                     print("用户名、密码错误三次，%s 账号已被锁定，请联系管理员解锁账号" % user_name)
                     allow_login = False
         else:
             print("用户名或密码有问题，请联系管理员处理")
             allow_login = False
+
+#登录成功后，可以进行下面的操作
+if login_successful:
+    pass
