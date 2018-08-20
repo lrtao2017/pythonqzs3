5ee579be2b098c52967cc46f198ca4c54857b228
lrtao2010 <lrtao2010@163.com>
2018-08-20 15:37:39 +0800

    201808201537

---
+++ app/Module_and_Functions/standard_library.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python 
+__author__ = "lrtao2010" 
+
+#python3.7 标准库
+
+#文本方面（text）
+
+#string
+import string
+
+s = 'The quick brown fox jumped over the lazy dog.'
+print(s)
+
+#capwords() 所有单词首字母大写
+# print(string.capwords(s))
+# The quick brown fox jumped over the lazy dog.
+# The Quick Brown Fox Jumped Over The Lazy Dog.
+
+
