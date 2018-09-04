1694d8f6d3302b99b54c3775d208cf5148fe87a0
lrtao2010 <lrtao2010@163.com>
2018-09-04 13:47:42 +0800

    add shutil module

---
+++ app/Module_and_Functions/shutil_module.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python 
+__author__ = "lrtao2010"
+
+#python 3.7 shutil模块
+#高级的文件、文件夹、压缩包处理模块
+
+import shutil
+
+#将文件json_text.txt内容拷贝到json_2.txt文件中
+#shutil.copyfileobj(open('json_text.txt','r'),open('json_2.txt','w'))
+
