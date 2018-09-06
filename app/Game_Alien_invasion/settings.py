3152c80d3ceea0186b54a9f8f82a2b519670e1e9
lrtao2010 <lrtao2010@163.com>
2018-09-06 13:16:12 +0800

    add ship

---
+++ app/Game_Alien_invasion/settings.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python 
+__author__ = "lrtao2010" 
+
+
+class Settings():
+    '''存放《外星人入侵》的所有设置的类'''
+    def __init__(self):
+        """初始化游戏设置"""
+        #屏幕设置
+        self.screen_width = 900
+        self.screen_height = 600
+        self.bg_color = (230,230,230)
---
+++ app/Game_Alien_invasion/ship.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python 
+__author__ = "lrtao2010"
+
+import pygame
+
+class Ship():
+    def __init__(self,screen):
+        """初始化飞船并设置其初始位置"""
+        self.screen = screen
+
