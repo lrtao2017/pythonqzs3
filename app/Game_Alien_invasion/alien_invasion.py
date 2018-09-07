f9e01281ee32d704160bf82e8f9a5b10f70ae49e
lrtao2010 <lrtao2010@163.com>
2018-09-07 15:02:10 +0800

    add alien

--- app/Game_Alien_invasion/alien_invasion.py
+++ app/Game_Alien_invasion/alien_invasion.py
@@ -20,12 +20,21 @@ def run_game():
     #创建一个用于存储子弹的编组
     bullets = Group()
 
+    #创建一个外星人编组
+    aliens = Group()
+
+    #创建一个外星人
+    #alien = Alien(ai_settings, screen)
+
+    # 创建一群外星人
+    gf.create_fleet(ai_settings, screen, aliens)
+
     #开始游戏的主循环
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, aliens,bullets)
 
 
 run_game()
--- app/Game_Alien_invasion/settings.py
+++ app/Game_Alien_invasion/settings.py
@@ -7,7 +7,7 @@ class Settings():
     def __init__(self):
         """初始化游戏设置"""
         #屏幕设置
-        self.screen_width = 900
+        self.screen_width = 960
         self.screen_height = 600
         self.bg_color = (230,230,230)
         #飞船设置
