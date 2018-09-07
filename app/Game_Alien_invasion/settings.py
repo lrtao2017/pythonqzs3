01d4cab1a3fc3257e941e58c12969ec4095560f3
lrtao2010 <lrtao2010@163.com>
2018-09-07 17:56:45 +0800

    add  alien move right or left or down

--- app/Game_Alien_invasion/settings.py
+++ app/Game_Alien_invasion/settings.py
@@ -23,6 +23,6 @@ class Settings():
 
         #外星人设置
         self.alien_speed_factor = 1
-        self.fleet_drop_speed = 2
+        self.fleet_drop_speed = 20
         #fleet_direction为1 表示向右移动，-1 表示向左移动
-        self.fleet_directhon = 1
+        self.fleet_direction = 1
