990787ce79a0cef46a89291ff0ad9454de8c98ea
lrtao2010 <lrtao2010@163.com>
2018-09-27 16:22:54 +0800

    add views

--- app/mysite/mysite/mysite/urls.py
+++ app/mysite/mysite/mysite/urls.py
@@ -13,10 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.conf.urls import url, include
+from django.contrib import admin
+
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^blog/', include('blog.urls', namespace='blog',app_name='blog')),
+    url(r'^blog/', include("blog.urls", namespace="blog",app_name="blog")),
+    #url(r'^blog/', include("blog.urls")),
 ]
